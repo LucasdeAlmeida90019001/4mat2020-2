27eeac68225053d51ef8a050be76aab922651fa5
LucasdeAlmeida90019001 <lucaseconomista9001@gmail.com>
2020-08-26 14:09:41 +0000

    (26/08) Instalação do mongoose para instalação do Mongo

---
+++ back-end/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+const db = require('./config/database')
+db ('mongodb+srv://lucas90019001:lucas90019001@cluster0.0ei6q.gcp.mongodb.net/agora_vai?retryWrites=true&w=majority')
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
